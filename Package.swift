03e060352fd9cca03bec1e4dcfb39eca0d9c63a1
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-02-02 21:10:11 +0800

    Account for change in name (`UpdatedSwiftUI` -> `Updating`)

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/KaiTheRedNinja/UpdatedSwiftUI.git", branch: "main")
+        .package(url: "https://github.com/KaiTheRedNinja/Updating.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +26,7 @@ let package = Package(
         .target(
             name: "AngularHeirarchy",
             dependencies: [
-                .product(name: "Updating", package: "UpdatedSwiftUI")
+                .product(name: "Updating", package: "Updating")
             ]),
         .testTarget(
             name: "AngularHeirarchyTests",
